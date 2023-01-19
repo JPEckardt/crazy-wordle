8bfc7927cd88f034d505156dd761797aed8cf055
Julian Eckardt <wwwadiosde@gmail.com>
2023-01-19 08:40:18 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { boardDefault, generateWordSet } from "./Words";
+import { createContext, useState, useEffect } from "react";
+import GameOver from "./components/GameOver";
+export const AppContext = createContext();
+
+function App() {
+  const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
+  const [wordSet, setWordset] = useState(new Set());
+  const [disabledLetters, setDisabledLetters] = useState([]);
+  const [correctWord, setCorrectWord] = useState("");
+  const [gameOver, setGameOver] = useState({
+    gameOver: false,
+    guessedWord: false,
+  });
+
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      setWordset(words.wordSet);
+      setCorrectWord(words.todaysWord);
+    });
+  }, []);
+
+  const onSelectLetter = (keyVal) => {
+    if (currAttempt.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+  };
+
+  const onDelete = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos - 1 });
+  };
+
+  const onEnter = () => {
+    if (currAttempt.letterPos !== 5) return;
+
+    let currWord = "";
+    for (let i = 0; i < 5; i++) {
+      currWord += board[currAttempt.attempt][i];
+    }
+
+    if (wordSet.has(currWord.toUpperCase())) {
+      setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+    } else {
+      alert("Word Not Found");
+    }
+
+    if (currWord.toUpperCase() === correctWord) {
+      setGameOver({ gameOver: true, guessedWord: true });
+      return;
+    }
+
+    if (currAttempt.attempt === 5)
+      setGameOver({ gameOver: true, guessedWord: false });
+  };
+  return (
+    <div className="App">
+      <nav>
+        <h1>Crazy Wordle</h1>
+      </nav>
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          currAttempt,
+          setCurrAttempt,
+          onSelectLetter,
+          onDelete,
+          onEnter,
+          correctWord,
+          setDisabledLetters,
+          disabledLetters,
+          setGameOver,
+          gameOver,
+        }}
+      >
+        <div className="game">
+          <Board />
+          {gameOver.gameOver ? <GameOver /> : <Keyboard />}
+        </div>
+      </AppContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GameOver.js
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { AppContext } from "../App";
+
+function GameOver() {
+  const { gameOver, setGameOver, correctWord, currAttempt } =
+    useContext(AppContext);
+  return (
+    <div className="gameOver">
+      <h3>
+        {gameOver.guessedWord
+          ? "Super du hast das Word erraten, wie toll!"
+          : "Haha, du hast verloren"}
+      </h3>
+      <h1>Richtig: {correctWord}</h1>
+      {gameOver.guessedWord && (
+        <h3>Du hast das Word in {currAttempt.attempt} Versuchen erraten </h3>
+      )}
+    </div>
+  );
+}
+
+export default GameOver;
